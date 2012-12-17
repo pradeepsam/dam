d89aef8f6e5c1ea4ae38e18304d5d2494905b693
unknown <mj802966@gdcwf714150.igategegdc.com>
2012-12-17 18:14:46 +0530

    checked in file notify api project using JNotify

---
+++ svc-dam-common-util/src/com/igate/dam/poller/service/JNotifyFilePoller.java
@@ -0,0 +1,119 @@
+package com.igate.dam.poller.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.igate.dam.poller.exception.DamJnotifyException;
+import com.igate.dam.poller.util.LoggerUtil;
+
+import net.contentobjects.jnotify.JNotify;
+import net.contentobjects.jnotify.JNotifyException;
+import net.contentobjects.jnotify.JNotifyListener;
+
+public class JNotifyFilePoller
+{
+	
+	LoggerUtil jNotifyLogger=new LoggerUtil();
+	
+	public void filePolling(int time,String path) throws DamJnotifyException {
+	
+		jNotifyLogger.display();
+	    // watch mask, specify events you care about, or JNotify.FILE_ANY for all events.
+	    int mask = JNotify.FILE_CREATED  | 
+	               JNotify.FILE_DELETED  | 
+	               JNotify.FILE_MODIFIED | 
+	               JNotify.FILE_RENAMED;
+
+	    // watch subtree?
+	    boolean watchSubtree = true;
+
+	    // add actual watch
+	 
+	    int watchID;
+		try {
+			
+			watchID = JNotify.addWatch(path,mask, watchSubtree, new Listener());
+		   
+
+	    // sleep a little, the application will exit if you
+	    // don't (watching is asynchronous), depending on your
+	    // application, this may not be required
+	    Thread.sleep(1000000);
+	    //(in order not to waste processing power by polling the directory)
+
+	    // to remove watch the watch
+	      boolean res = JNotify.removeWatch(watchID);
+	       if (!res) {
+	      // invalid watch ID specified.
+	           }
+	       }
+	   catch (JNotifyException e)
+	   {
+		jNotifyLogger.jnotifyexception();
+		throw new DamJnotifyException("Exception occured while Filepolling");
+	   } 
+	   catch (InterruptedException e)
+	   {
+		jNotifyLogger.interruptedException();
+		throw new DamJnotifyException("Interruption occured while processing");
+		
+	    }
+	  }
+	  class Listener implements JNotifyListener 
+	  {
+	
+	    public void fileRenamed(int wd, String rootPath, String oldName,String newName) 
+	    {
+	    	
+	    
+	      jNotifyLogger.renameFileLog(rootPath, oldName, newName);
+	    }
+	    public void fileModified(int wd, String rootPath, String name) {
+	    	
+	  
+	    	jNotifyLogger.fileModifiedLog(wd, rootPath, name);
+	    }
+	    public void fileDeleted(int wd, String rootPath, String name) {
+	 
+	      jNotifyLogger.fileDeletedLog(wd, rootPath, name);
+	    }
+	    public void fileCreated(int wd, String rootPath, String name) 
+	    {
+	   
+	      jNotifyLogger.createFileLog(rootPath, name);
+	      
+	    }
+	    void print(String msg) {
+	      System.err.println(msg);
+	    }
+	  }
+	
+	  /*************************Loading the Property File******************************/
+	  
+	  
+	 public int loadPropertyFile()throws DamJnotifyException
+	 {
+		Properties properties=new Properties();
+		try
+		{
+		properties.load(new FileInputStream(new File("resources\\pollingInterval.properties")));
+		jNotifyLogger.loadPropertyFile();
+		String str=properties.getProperty("pollingInterval");
+		int time=Integer.parseInt(str);
+		return time;
+		}
+		catch (FileNotFoundException fileNotFoundException) {
+			
+			throw new DamJnotifyException("unable to find the property file");
+		} catch (IOException e) {
+			throw new DamJnotifyException("unable to load the property file");
+		}
+		
+		
+		 
+	 }
+	
+}
---
+++ svc-dam-common-util/src/com/igate/dam/poller/util/LoggerUtil.java
@@ -0,0 +1,59 @@
+package com.igate.dam.poller.util;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+
+
+
+
+public class LoggerUtil {
+
+
+	public static final Logger logger = Logger.getLogger(LoggerUtil.class);
+	 
+	public LoggerUtil()
+	{
+		  PropertyConfigurator.configure("resources\\log4j.properties");
+	}
+	 public void loadDirectoryName(String directoryName)
+     {
+   	  logger.info("The directory chosen for Monitoring:"+directoryName);
+     }
+	  public void loadPropertyFile()
+      {
+	  logger.info("property file loaded");
+      }
+	    public void renameFileLog( String rootPath, String oldName,
+				String newName)
+	  {
+		  logger.info("renamed " + rootPath + " : " + oldName + " -> " + newName);
+	  }
+	  
+	  public void createFileLog( String rootPath,String name)
+	  {
+		  logger.info("created " + rootPath + " : " + name);
+	  }
+	  
+	  public void fileModifiedLog(int wd, String rootPath, String name)
+	  {
+		  logger.info("modified " + rootPath + " : " + name);
+			
+	  }
+      public void fileDeletedLog(int wd, String rootPath, String name)
+	   {
+		   logger.info("deleted " + rootPath + " : " + name);
+	   }
+     public void jnotifyexception()
+     {
+    	 logger.info("Exception occured while Filepolling");
+     }
+     public void interruptedException()
+     {
+    	 logger.info("Interruption occured while processing");
+     }
+     public void display()
+     {
+    	 logger.info(" jnotify poller started monitoring...");
+     }
+}
